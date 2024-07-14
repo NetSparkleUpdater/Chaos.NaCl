317728758d552f9dfae8a38bf2b40a531f977660
Deadpikle <mbabienco@southern.edu>
2024-07-14 14:33:28 +0900

    Tweak usings for Ed25519 tests

--- Chaos.NaCl.Tests/Ed25519Tests.cs
+++ Chaos.NaCl.Tests/Ed25519Tests.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Chaos.NaCl.Tests
 {
